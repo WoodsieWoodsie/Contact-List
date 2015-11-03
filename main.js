bf8dbc8f3a72ac2dd1d47f775be54687a174a09e
Rachel Wood <r@codinghouse.co>
2015-11-02 17:53:09 -0800

    Allow input value fetching

--- main.js
+++ main.js
@@ -2,9 +2,32 @@
 
 (function(){
 
+  $('.add').click(addContact);
 
+  var entries = JSON.parse(localStorage.entries || "[]");
 
+  function addContact() {
+    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    console.log('name:', name)
+    nameInput.value = '';
 
+    var emailInput = document.getElementById('email');
+    var email = emailInput.value;
+    console.log('email:', email);
+    emailInput.value = '';
+
+    var phoneInput = document.getElementById('phone');
+    var phone = phoneInput.value;
+    console.log('phone:', phone);
+    phoneInput.value = '';
+
+    var twitterInput = document.getElementById('twitter');
+    var twitter = twitterInput.value;
+    console.log('twitter:', twitter);
+    twitterInput.value = '';
+  }
 
 
 
